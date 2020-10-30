eed37d3ad6de505c1f282ec5dec30e93407be438
Yoel Yukalo <yoelyukalo@gmail.com>
2020-10-30 18:04:08 -0500

    not working, almost done

---
+++ app/controllers/hello_worlds_controller.rb
@@ -0,0 +1,6 @@
+class HelloWorldsController < ApplicationController
+
+    def hello_world
+        render "hello_world"
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  
+  get '/hello_world' => 'hello_worlds#hello_world'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
